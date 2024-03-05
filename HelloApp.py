501990184a4299d4724d6114fd0ae90859142c75
Alexander <azazf93@gmail.com>
2024-03-05 11:29:54 +0300

    Add solution

--- HelloApp.py
+++ HelloApp.py
@@ -1,2 +1,6 @@
 print("Hello Python from Visual Studio Code!")
 print("Hello world!")
+# Вывести на экран пять строк из нулей, количество нулей в каждой строке равно номеру строки, 
+# нули между собой разделять точкой с запятой. 
+for i in range(6):
+    print(*['0']*i, sep=';')
